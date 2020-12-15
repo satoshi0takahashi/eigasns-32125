388c854fee21a21a4ced6f0ffbbe46cc2f9443dc
satoshi0takahashi <v.c.2000@icloud.com>
2020-12-15 18:23:11 +0900

    ユーザー登録機能

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    private
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    end
+  end
 end
