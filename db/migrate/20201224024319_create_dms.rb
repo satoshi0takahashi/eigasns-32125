8dc35b2127586abfd2b540b9a84ac2edcd43964e
satoshi0takahashi <v.c.2000@icloud.com>
2020-12-24 11:48:52 +0900

    マイグレーションファイル記述

--- db/migrate/20201224024319_create_dms.rb
+++ db/migrate/20201224024319_create_dms.rb
@@ -1,7 +1,7 @@
 class CreateDms < ActiveRecord::Migration[6.0]
   def change
     create_table :dms do |t|
-
+      t.string :name, null: false
       t.timestamps
     end
   end
--- db/migrate/20201224024434_create_dm_users.rb
+++ db/migrate/20201224024434_create_dm_users.rb
@@ -1,7 +1,8 @@
 class CreateDmUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :dm_users do |t|
-
+      t.references :dm, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
